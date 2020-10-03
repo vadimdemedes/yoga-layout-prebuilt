6b33b505ccb77a16e2fd5779349e7c7f443dac84
Vadim Demedes <vdemedes@gmail.com>
2020-10-03 17:50:55 +0300

    Update build script to work on Node.js v12.x

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Fail when any command below fails
+set -e
+
 npm rm yoga-layout
-npm i --no-save yoga-layout
+
+# Install yoga-layout, but without running its build script on install
+npm i --no-save --ignore-scripts yoga-layout
+
+# Replace nbind with the version in master branch
+cd node_modules/yoga-layout
+npm rm nbind
+npm i charto/nbind
+npm run build:node
+cd -
+
+# Copy built files
 rm -rf yoga-layout
 mkdir -p yoga-layout
 mkdir -p yoga-layout/dist
